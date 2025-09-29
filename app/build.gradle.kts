fe1b8e58257f5dfa4a8f3e5ab7cd6335f559bf14
zhaiyundong <dongDONG!@#>
2025-09-29 10:11:49 +0800

    优化代码

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.adbid.sdk"
-        //applicationId = "com.moguo.farmGameUnique"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -18,33 +17,6 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    signingConfigs {
-        create("mySign") { // 或者使用 create("debug") 如果不存在名为 "debug" 的配置
-            storeFile = file("./key/ydyFarm.keystore")
-            storePassword = "123456"
-            keyAlias = "aprilIdiom"
-            keyPassword = "123456"
-            enableV2Signing = true // 在 Kotlin DSL 中，v2SigningEnabled 变为 enableV2Signing
-        }
-    }
-
-    buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs["mySign"]
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
-        }
-
-        getByName("debug") {
-            signingConfig = signingConfigs["mySign"]
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
