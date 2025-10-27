a4bed09f4feec1f7a706f15dca7bfafa72aa96ce
zhaiyundong <dongDONG!@#>
2025-10-27 19:32:13 +0800

    优化广告依赖

--- app/src/main/java/com/yiman/ad/adbid/BaseActivity.java
+++ app/src/main/java/com/yiman/ad/adbid/BaseActivity.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 
 import androidx.activity.ComponentActivity;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-import com.ax.ad.cpc.util.immersionbar.ImmersionBar;
+import com.adbid.adx.util.immersionbar.ImmersionBar;
+
 
 public class BaseActivity extends ComponentActivity {
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
